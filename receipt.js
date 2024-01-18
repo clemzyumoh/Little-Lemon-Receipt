3a6b7fe76f29d0e38c6ab6649685ba69d079ce62
clemzyumoh <142527861+clemzyumoh@users.noreply.github.com>
2024-01-18 04:37:04 -0800

    Update receipt.js

---
+++ receipt.js
@@ -0,0 +1,66 @@
+
+
+
+// FINAL PROJECT
+// Given variables
+const dishData = [
+    {
+        name: "Italian pasta",
+        price: 9.55
+    },
+    {
+        name: "Rice with veggies",
+        price: 8.65
+    },
+    {
+        name: "Chicken with potatoes",
+        price: 15.55
+    },
+    {
+        name: "Vegetarian Pizza",
+        price: 6.45
+    },
+ ]
+ const tax = 1.20;
+
+// Implement getPrices()
+ function getPrices(taxBoolean) {
+    for (let i = 0; i < dishData.length; i++){
+        const obj = dishData[i];
+        var finalPrice;
+        if (taxBoolean=== true) {
+            finalPrice = tax * obj.price
+        } else if (taxBoolean === false) {
+            finalPrice = obj.price
+        } else{
+              console.log("You need to pass a boolean to the getPrices call!")
+              return`(to "jump out" of the further function execution)`
+        }
+        console.log(`Dish: ${obj.name} Price: $${finalPrice}`);
+    };
+ }
+ getPrices(true)
+ 
+ getPrices(false)
+ 
+ getPrices(4)
+// Implement getDiscount()
+ function getDiscount(taxBoolean, guests) {
+    getPrices(taxBoolean)
+    if (typeof (guests) === "number" && (0 < guests < 30)) {
+        let discount = 0
+        if (guests < 5) {
+            discount = 5
+        } else if (guests >= 5) {
+            discount = 10
+        }
+        return console.log(`Discount is: $${discount} `);
+    } else {
+        return console.log('The second argument must be a number between 0 and 30')
+    }
+ }
+
+
+// Call getDiscount()
+ getDiscount(true, 2);
+getDiscount(false, 10);
